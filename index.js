d272de440a24b280b4ae6673d429a718b1b27110
Akhileswar <akhileswarsathivada@gmail.com>
2023-02-16 19:32:28 +0530

    localstorage

--- index.js
+++ index.js
@@ -1,135 +1,140 @@
-window.addEventListener("load", () => {
-  let i = localStorage.length - 1;
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task");
-  const list = document.querySelector("#tasks");
+const taskForm = document.querySelector("#new-task-form");
+const task = document.querySelector("#new-task");
+const taskList = document.querySelector("#tasks");
 
-  // Load Tasks from localStorage
-  for (let j = 0; j < localStorage.length + 1; j++) {
-    const task = localStorage.getItem(j);
-    if (!task) continue;
+const createTask = (event) => {
+  event.preventDefault();
 
-    // List
-    const task_el = document.createElement("div");
-    task_el.classList.add("task");
-
-    // Content class
-    const task_content = document.createElement("div");
-    task_content.classList.add("content");
-    task_el.appendChild(task_content); //Appending Content to Task
-
-    // Input
-    const task_input = document.createElement("input");
-    task_input.classList.add("text");
-    task_input.type = "text";
-    task_input.value = task;
-    task_input.setAttribute("readonly", "readonly");
-
-    task_content.appendChild(task_input); //Appending Input to Content
-
-    // Actions Class
-    const task_actions = document.createElement("div");
-    task_actions.classList.add("actions");
-
-    // Edit button
-    const edit_btn = document.createElement("button");
-    edit_btn.classList.add("edit");
-    edit_btn.innerHTML = "Edit";
-
-    // Delete button
-    const delete_btn = document.createElement("button");
-    delete_btn.classList.add("delete");
-    delete_btn.innerHTML = "Delete";
-
-    task_actions.appendChild(edit_btn); //Appending Button to Actions
-    task_actions.appendChild(delete_btn); //Appending Button to Actions
-
-    task_el.appendChild(task_actions); //Appending Actions to Tasks
-
-    list.appendChild(task_el);
-
-    edit_btn.addEventListener("click", () => {
-      if (edit_btn.innerHTML == "edit") {
-        task_input.removeAttribute("readonly");
-        task_input.focus();
-        edit_btn.innerText = "save";
-      } else {
-        task_input.setAttribute("readonly", "readonly");
-        localStorage.setItem(j, task_input.value);
-        edit_btn.innerText = "edit";
-      }
-    });
-
-    delete_btn.addEventListener("click", () => {
-      localStorage.removeItem(j);
-      list.removeChild(task_el);
-    });
+  if (!task.value) {
+    alert("Enter something");
+    return;
   }
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const task = input.value;
-    if (task == "") {
-      alert("Please add something");
-      return;
+  const taskId = Date.now(); // Generate a unique id for the task
+  localStorage.setItem(taskId, task.value);
+
+  // Task Container
+  const taskContainer = document.createElement("div");
+  taskContainer.className = "task";
+
+  // Task
+  const taskInput = document.createElement("input");
+  taskInput.classList.add("text");
+  taskInput.type = "text";
+  taskInput.value = task.value;
+  taskInput.setAttribute("readonly", "readonly");
+
+  //   Actions
+  const taskActions = document.createElement("div");
+  taskActions.className = "actions";
+  // Edit Button
+  const editBtn = document.createElement("button");
+  editBtn.className = "edit";
+  editBtn.innerHTML = "Edit";
+  // Delete Button
+  const delBtn = document.createElement("button");
+  delBtn.className = "delete";
+  delBtn.innerHTML = "Delete";
+  // Appending buttons into Task actions
+  taskActions.appendChild(editBtn);
+  taskActions.appendChild(delBtn);
+
+  //   Task into task container
+  taskContainer.appendChild(taskInput);
+  // Appending Task Actions into task container
+  taskContainer.appendChild(taskActions);
+
+  // Task Container into task box
+  taskList.append(taskContainer);
+
+  task.value = "";
+
+  editBtn.addEventListener("click", () => {
+    if (editBtn.innerHTML == "edit") {
+      taskInput.removeAttribute("readonly");
+      taskInput.focus();
+      editBtn.innerText = "save";
+    } else {
+      taskInput.setAttribute("readonly", "readonly");
+      editBtn.innerText = "edit";
     }
-
-    i++;
-    localStorage.setItem(i, task);
-    // List
-    const task_el = document.createElement("div");
-    task_el.classList.add("task");
-
-    // Content class
-    const task_content = document.createElement("div");
-    task_content.classList.add("content");
-    task_el.appendChild(task_content); //Appending Content to Task
-
-    // Input
-    const task_input = document.createElement("input");
-    task_input.classList.add("text");
-    task_input.type = "text";
-    task_input.value = task;
-    task_input.setAttribute("readonly", "readonly");
-
-    task_content.appendChild(task_input); //Appending Input to Content
-
-    // Actions Class
-    const task_actions = document.createElement("div");
-    task_actions.classList.add("actions");
-
-    // Edit button
-    const edit_btn = document.createElement("button");
-    edit_btn.classList.add("edit");
-    edit_btn.innerHTML = "Edit";
-
-    // Delete button
-    const delete_btn = document.createElement("button");
-    delete_btn.classList.add("delete");
-    delete_btn.innerHTML = "Delete";
-
-    task_actions.appendChild(edit_btn); //Appending Button to Actions
-    task_actions.appendChild(delete_btn); //Appending Button to Actions
-
-    task_el.appendChild(task_actions); //Appending Actions to Tasks
-
-    list.appendChild(task_el);
-
-    input.value = "";
-
-    edit_btn.addEventListener("click", () => {
-      if (edit_btn.innerHTML == "edit") {
-        task_input.removeAttribute("readonly");
-        task_input.focus();
-        edit_btn.innerText = "save";
-      } else {
-        task_input.setAttribute("readonly", "readonly");
-        edit_btn.innerText = "edit";
-      }
-    });
-
-    delete_btn.addEventListener("click", () => {
-      list.removeChild(task_el);
-    });
   });
+
+  delBtn.addEventListener("click", () => {
+    taskList.removeChild(taskContainer);
+    localStorage.removeItem(taskId);
+    tasks--;
+  });
+};
+
+window.addEventListener("load", () => {
+  // event.preventDefault();
+  const tasks = localStorage.length;
+  for (let i = 0; i < localStorage.length + 1; i++) {
+    console.log(localStorage.key(i));
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    // const taskValue = localStorage.getItem;
+    if (localStorage.getItem(localStorage.key(i)) !== null) {
+      // Task Container
+      const taskContainer = document.createElement("div");
+      taskContainer.className = "task";
+      taskContainer.id = key;
+
+      // Task
+      const taskInput = document.createElement("input");
+      taskInput.classList.add("text");
+      taskInput.type = "text";
+      taskInput.value = value;
+      taskInput.setAttribute("readonly", "readonly");
+
+      //   Actions
+      const taskActions = document.createElement("div");
+      taskActions.className = "actions";
+      // Edit Button
+      const editBtn = document.createElement("button");
+      editBtn.className = "edit";
+      editBtn.innerHTML = "Edit";
+      // Delete Button
+      const delBtn = document.createElement("button");
+      delBtn.className = "delete";
+      delBtn.innerHTML = "Delete";
+      // Appending buttons into Task actions
+      taskActions.appendChild(editBtn);
+      taskActions.appendChild(delBtn);
+
+      //   Task into task container
+      taskContainer.appendChild(taskInput);
+      // Appending Task Actions into task container
+      taskContainer.appendChild(taskActions);
+
+      // Task Container into task box
+      taskList.append(taskContainer);
+
+      task.value = "";
+
+      editBtn.addEventListener("click", () => {
+        if (editBtn.innerHTML == "edit") {
+          taskInput.removeAttribute("readonly");
+          taskInput.focus();
+          editBtn.innerText = "save";
+        } else {
+          taskInput.setAttribute("readonly", "readonly");
+          localStorage.setItem(key, taskInput.value);
+          editBtn.innerText = "edit";
+        }
+      });
+
+      delBtn.addEventListener("click", () => {
+        localStorage.removeItem(key);
+        console.log(key);
+        taskList.removeChild(taskContainer);
+        updateLocalStorage();
+        tasks--;
+      });
+    }
+  }
+  // localStorage.clear();
 });
+
+taskForm.addEventListener("submit", createTask);
